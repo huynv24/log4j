49196a4c0af375116b9a8a51c68fccb49d58ee2f
huynv24 <72879056+huynv24@users.noreply.github.com>
2022-05-25 09:05:10 +0700

    Create main.java

---
+++ main.java
@@ -0,0 +1,11 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class main {
+    private static final Logger logger = LogManager.getLogger(main.class);
+    public static void main(String[] args) {
+        logger.debug("Hello from Log4j 2");
+        logger.debug("Hello from Log4j 2");
+
+    }
+}
